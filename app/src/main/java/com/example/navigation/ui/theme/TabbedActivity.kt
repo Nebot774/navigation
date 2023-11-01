2b1cbeecbf116eb23313c9f33571bc766f82cc42
Nebot774 <adriclemenbot@gmail.com>
2023-11-01 20:27:53 +0100

    problema del tema solucionado

---
+++ app/src/main/java/com/example/navigation/ui/theme/TabbedActivity.kt
@@ -0,0 +1,12 @@
+package com.example.navigation.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.navigation.R
+
+class TabbedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabbed)
+    }
+}
